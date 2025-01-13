370731e99223cdad0df75090629b3f8bd85ef5f6
theriseof9 <h2010155@nushigh.edu.sg>
2025-01-13 09:23:13 +0800

    new imple

--- source/ronin_resnet_ode.py
+++ source/ronin_resnet_ode.py
@@ -1,271 +1,104 @@
-"""
-The code is based on the original ResNet implementation from torchvision.models.resnet
-"""
-
 import torch
 import torch.nn as nn
 from torchdiffeq import odeint
 
+def add_time(x, t):
+    bs, c, l = x.shape
+    t_expanded = t.expand(bs, 1, l)
+    xt = torch.cat([x, t_expanded], dim=1)
+    return xt
 
-def conv3(in_planes, out_planes, kernel_size, stride=1, dilation=1):
-    return nn.Conv1d(in_planes, out_planes, kernel_size=kernel_size, stride=stride,
-                     padding=kernel_size // 2, bias=False)
-
-
-class BasicBlock1D(nn.Module):
-    expansion = 1
-
-    def __init__(self, in_planes, out_planes, kernel_size, stride=1, dilation=1, downsample=None):
-        super(BasicBlock1D, self).__init__()
-        self.conv1 = conv3(in_planes, out_planes, kernel_size, stride, dilation)
-        self.bn1 = nn.BatchNorm1d(out_planes)
+class Conv1dODEFunc(nn.Module):
+    def __init__(self, dim, kernel_size=3):
+        super(Conv1dODEFunc, self).__init__()
+        self.conv1 = nn.Conv1d(dim + 1, dim, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
+        self.bn1 = nn.BatchNorm1d(dim)
         self.relu = nn.ReLU(inplace=True)
-        self.conv2 = conv3(out_planes, out_planes, kernel_size)
-        self.bn2 = nn.BatchNorm1d(out_planes)
-        self.stride = stride
-        self.downsample = downsample
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
-class Bottleneck1D(nn.Module):
-    expansion = 4
-
-    def __init__(self, in_planes, out_planes, kernel_size, stride=1, dilation=1, downsample=None):
-        super(Bottleneck1D, self).__init__()
-        self.conv1 = nn.Conv1d(in_planes, out_planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm1d(out_planes)
-        self.conv2 = conv3(out_planes, out_planes, kernel_size, stride, dilation)
-        self.bn2 = nn.BatchNorm1d(out_planes)
-        self.conv3 = nn.Conv1d(out_planes, out_planes * self.expansion, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm1d(out_planes * self.expansion)
-        self.relu = nn.ReLU(inplace=True)
-        self.stride = stride
-        self.downsample = downsample
-
-    def forward(self, x):
-        residual = x
-
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = self.relu(out)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-        out = self.relu(out)
-
-        out = self.conv3(out)
-        out = self.bn3(out)
-
-        if self.downsample is not None:
-            residual = self.downsample(x)
-
-        out += residual
-        out = self.relu(out)
-
-        return out
-
-
-def add_time_1d(in_tensor, t):
-    bs, c, seq_len = in_tensor.shape
-
-    # Check if `t` is a scalar. If so, convert it to a tensor with proper shape
-    if not torch.is_tensor(t):
-        t = torch.tensor([t], dtype=in_tensor.dtype, device=in_tensor.device)
-
-    # Verify `t` is of the right shape, i.e., [1] or [bs]
-    if t.dim() == 0:
-        # If t is a scalar, repeat it for each batch
-        t = t.repeat(bs)
-
-    # Now expand `t` to match the desired dimensions
-    time_channel = t.view(bs, 1, 1).expand(bs, 1, seq_len)
-
-    # Concatenate the new time channel to the original input on the channel dimension
-    return torch.cat((in_tensor, time_channel), dim=1)
-
-
-class ODEFunc1D(nn.Module):
-    def __init__(self, in_planes, out_planes):
-        super(ODEFunc1D, self).__init__()
-        self.conv = nn.Conv1d(in_planes, out_planes, kernel_size=3, padding=1, bias=False)
-        self.norm = nn.BatchNorm1d(out_planes)
-        self.relu = nn.ReLU(inplace=True)
-
+        self.conv2 = nn.Conv1d(dim + 1, dim, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
+        self.bn2 = nn.BatchNorm1d(dim)
 
     def forward(self, t, x):
-        xt = add_time_1d(x, t)
-        h = self.norm(self.relu(self.conv(xt)))
-        ht = add_time_1d(h, t)
-        dxdt = self.norm(self.relu(self.conv(ht)))
-        return dxdt
-
-
-"""class ConvODEF(ODEF):
-    def __init__(self, dim):
-        super(ConvODEF, self).__init__()
-        self.conv1 = conv3x3(dim + 1, dim)
-        self.norm1 = norm(dim)
-        self.conv2 = conv3x3(dim + 1, dim)
-        self.norm2 = norm(dim)
-
-    def forward(self, x, t):
         xt = add_time(x, t)
-        h = self.norm1(torch.relu(self.conv1(xt)))
-        ht = add_time(h, t)
-        dxdt = self.norm2(torch.relu(self.conv2(ht)))
+        out = self.conv1(xt)
+        out = self.bn1(out)
+        out = self.relu(out)
+        ht = add_time(out, t)
+        dxdt = self.conv2(ht)
+        dxdt = self.bn2(dxdt)
         return dxdt
-"""
 
-class ODEBlock1D(nn.Module):
-    def __init__(self, odefunc):
-        super(ODEBlock1D, self).__init__()
+class ODEBlock(nn.Module):
+    def __init__(self, odefunc, integration_time=torch.tensor([0, 1]).float()):
+        super(ODEBlock, self).__init__()
         self.odefunc = odefunc
-        self.integration_time = torch.tensor([0, 1]).float()
+        self.integration_time = integration_time
 
     def forward(self, x):
         self.integration_time = self.integration_time.type_as(x)
-        out = odeint(self.odefunc, x, self.integration_time, method='rk4')[1]
-        return out
-
-
-class FCOutputModule(nn.Module):
-    """
-    Fully connected output module.
-    """
-    def __init__(self, in_planes, num_outputs, **kwargs):
-        """
-        Constructor for a fully connected output layer.
-
-        Args:
-          in_planes: number of planes (channels) of the layer immediately proceeding the output module.
-          num_outputs: number of output predictions.
-          fc_dim: dimension of the fully connected layer.
-          dropout: the keep probability of the dropout layer
-          trans_planes: (optional) number of planes of the transition convolutional layer.
-        """
-        super(FCOutputModule, self).__init__()
-        fc_dim = kwargs.get('fc_dim', 1024)
-        dropout = kwargs.get('dropout', 0.5)
-        in_dim = kwargs.get('in_dim', 7)
-        trans_planes = kwargs.get('trans_planes', None)
-        if trans_planes is not None:
-            self.transition = nn.Sequential(
-                nn.Conv1d(in_planes, trans_planes, kernel_size=1, bias=False),
-                nn.BatchNorm1d(trans_planes))
-            in_planes = trans_planes
-        else:
-            self.transition = None
-
-        self.fc = nn.Sequential(
-            nn.Linear(in_planes * in_dim, fc_dim),
-            nn.ReLU(True),
-            nn.Dropout(dropout),
-            nn.Linear(fc_dim, fc_dim),
-            nn.ReLU(True),
-            nn.Dropout(dropout),
-            nn.Linear(fc_dim, num_outputs))
-
-    def get_dropout(self):
-        return [m for m in self.fc if isinstance(m, torch.nn.Dropout)]
-
-    def forward(self, x):
-        if self.transition is not None:
-            x = self.transition(x)
-        x = x.view(x.size(0), -1)
-        y = self.fc(x)
-        return y
-
+        out = odeint(self.odefunc, x, self.integration_time, method='rk4')
+        return out[1]
 
 class GlobAvgOutputModule(nn.Module):
-    """
-    Global average output module.
-    """
+    """ Global average output module. """
     def __init__(self, in_planes, num_outputs):
         super(GlobAvgOutputModule, self).__init__()
         self.avg = nn.AdaptiveAvgPool1d(1)
         self.fc = nn.Linear(in_planes, num_outputs)
-
-    def get_dropout(self):
-        return []
-
+    
     def forward(self, x):
-        x = self.avg()
+        x = self.avg(x)
         x = x.view(x.size(0), -1)
         return self.fc(x)
 
-
 class ResNet1D(nn.Module):
-    def __init__(self, num_inputs, num_outputs, block_type, group_sizes, base_plane=64, output_block=None,
-                 zero_init_residual=False, **kwargs):
+    def __init__(self, num_inputs, num_outputs, base_plane=64, output_block=None, zero_init_residual=False, **kwargs):
         super(ResNet1D, self).__init__()
         self.base_plane = base_plane
         self.inplanes = self.base_plane
-
-        # Input module
+        # Input Module
         self.input_block = nn.Sequential(
             nn.Conv1d(num_inputs, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False),
             nn.BatchNorm1d(self.inplanes),
             nn.ReLU(inplace=True),
             nn.MaxPool1d(kernel_size=3, stride=2, padding=1)
         )
-
-        # Residual groups
-        self.planes = [self.base_plane * (2 ** i) for i in range(len(group_sizes))]
+        # ODE Blocks
+        self.planes = [self.base_plane * (2 ** i) for i in range(4)]
         kernel_size = kwargs.get('kernel_size', 3)
-        strides = [1] + [2] * (len(group_sizes) - 1)
-        dilations = [1] * len(group_sizes)
-        groups = [self._make_ode_group1d(self.planes[i], self.planes[i] * block_type.expansion, group_sizes[i])
-                  for i in range(len(group_sizes))]
+        groups = []
+        in_planes = self.inplanes
+        for i, planes in enumerate(self.planes):
+            stride = 1 if i == 0 else 2
+            layer = self._make_ode_layer(in_planes, planes, kernel_size, stride)
+            groups.append(layer)
+            in_planes = planes
         self.residual_groups = nn.Sequential(*groups)
-
-        # Output module
+        self.inplanes = in_planes  # Update self.inplanes
+        # Output Module
         if output_block is None:
-            self.output_block = GlobAvgOutputModule(self.planes[-1] * block_type.expansion, num_outputs)
+            self.output_block = GlobAvgOutputModule(self.inplanes, num_outputs)
         else:
-            self.output_block = output_block(self.planes[-1] * block_type.expansion, num_outputs, **kwargs)
-
+            self.output_block = output_block(self.inplanes, num_outputs, **kwargs)
         self._initialize(zero_init_residual)
 
-    # def _make_residual_group1d(self, block_type, planes, kernel_size, blocks, stride=1, dilation=1):
-    #     downsample = None
-    #     if stride != 1 or self.inplanes != planes * block_type.expansion:
-    #         downsample = nn.Sequential(
-    #             nn.Conv1d(self.inplanes, planes * block_type.expansion, kernel_size=1, stride=stride, bias=False),
-    #             nn.BatchNorm1d(planes * block_type.expansion))
-    #     layers = []
-    #     layers.append(block_type(self.inplanes, planes, kernel_size=kernel_size,
-    #                              stride=stride, dilation=dilation, downsample=downsample))
-    #     self.inplanes = planes * block_type.expansion
-    #     for _ in range(1, blocks):
-    #         layers.append(block_type(self.inplanes, planes, kernel_size=kernel_size))
-    #
-    #     return nn.Sequential(*layers)
+    def _make_ode_layer(self, in_planes, out_planes, kernel_size, stride=1):
+        if stride != 1 or in_planes != out_planes:
+            prelayer = nn.Sequential(
+                nn.Conv1d(in_planes, out_planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm1d(out_planes),
+                nn.ReLU(inplace=True)
+            )
+        else:
+            prelayer = nn.Identity()
 
-    def _make_ode_group1d(self, in_planes, out_planes, blocks):
-        layers = []
-        current_planes = in_planes+1  # Add 1 to account for the time channel
-        for _ in range(blocks):
-            odefunc = ODEFunc1D(current_planes, out_planes)
-            layers.append(ODEBlock1D(odefunc))
-            current_planes = out_planes  # Update current_planes for subsequent layers
-        return nn.Sequential(*layers)
+        odefunc = Conv1dODEFunc(out_planes, kernel_size)
+        odeblock = ODEBlock(odefunc)
+
+        layer = nn.Sequential(
+            prelayer,
+            odeblock
+        )
+        return layer
 
     def _initialize(self, zero_init_residual):
         for m in self.modules():
@@ -277,22 +110,14 @@ class ResNet1D(nn.Module):
             elif isinstance(m, nn.Linear):
                 nn.init.normal_(m.weight, 0, 0.01)
                 nn.init.constant_(m.bias, 0)
-
-        # Zero-initialize the last BN in each residual branch,
-        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
-        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        # Zero-initialize the last BN in ODE functions
         if zero_init_residual:
             for m in self.modules():
-                if isinstance(m, Bottleneck1D):
-                    nn.init.constant_(m.bn3.weight, 0)
-                elif isinstance(m, BasicBlock1D):
-                    nn.init.constant_(m.bn2.weight, 0)
+                if isinstance(m, Conv1dODEFunc):
+                    nn.init.constant_(m.bn2.weight, 0)  # Zero-initialize bn2 weight
 
     def forward(self, x):
         x = self.input_block(x)
         x = self.residual_groups(x)
         x = self.output_block(x)
         return x
-
-    def get_num_params(self):
-        return sum(p.numel() for p in self.parameters() if p.requires_grad)
